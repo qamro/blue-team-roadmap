24878a094d75b1defb5bcad170911967dc4030a5
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-07-22 18:55:06 +0200

    fic some shits

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -68,7 +68,11 @@ export default function Footer() {
             ))}
           </div>
           <div className="text-xs text-blue-400/20">
-            Developed by <span style="color: red;">Bakhouche Mohamed Qamar Eddine.</span>
+            Developed by 
+            <span style={{ color: "red" }}>
+              This sentence is red.
+            </span>{" "}
+
           </div>
         </div>
       </div>
